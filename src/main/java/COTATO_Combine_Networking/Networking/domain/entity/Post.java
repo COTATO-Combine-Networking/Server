ff25a75219b5f09cf50bc6dd167067233cc5cad4
YoungjaeRo <ryj0330@naver.com>
2025-05-06 12:41:57 +0900

    feat : 예시 Entity 구현

---
+++ src/main/java/COTATO_Combine_Networking/Networking/domain/entity/Post.java
@@ -0,0 +1,38 @@
+package COTATO_Combine_Networking.Networking.domain.entity;
+
+import COTATO_Combine_Networking.Networking.domain.enums.PostCategory;
+import COTATO_Combine_Networking.Networking.global.entity.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Post extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private PostCategory category;
+
+    @Builder
+    public Post(String title, String content, PostCategory category) {
+        this.title = title;
+        this.content = content;
+        this.category = category;
+    }
+}
