9bcd145908c1beea1ae8e4fce802f11c239a05c5
YoungjaeRo <ryj0330@naver.com>
2025-05-06 12:42:33 +0900

    feat : 예시 converter 구현

---
+++ src/main/java/COTATO_Combine_Networking/Networking/domain/converter/PostConverter.java
@@ -0,0 +1,25 @@
+package COTATO_Combine_Networking.Networking.domain.converter;
+
+import COTATO_Combine_Networking.Networking.domain.dto.request.PostCreateRequest;
+import COTATO_Combine_Networking.Networking.domain.dto.response.PostResponse;
+import COTATO_Combine_Networking.Networking.domain.entity.Post;
+
+public class PostConverter {
+    public static Post toEntity(PostCreateRequest request) {
+        return Post.builder()
+            .title(request.getTitle())
+            .content(request.getContent())
+            .category(request.getCategory())
+            .build();
+    }
+
+    public static PostResponse toResponse(Post post) {
+        return PostResponse.builder()
+            .id(post.getId())
+            .title(post.getTitle())
+            .content(post.getContent())
+            .category(post.getCategory())
+            .createdAt(post.getCreatedAt())
+            .build();
+    }
+}
