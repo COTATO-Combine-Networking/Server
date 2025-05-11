b709fcbf34070b786ce7cd9b235e2e613ba17dfa
marshmallowing <114673063+marshmallowing@users.noreply.github.com>
2025-05-11 21:48:53 +0900

    feat: place entity

---
+++ src/main/java/COTATO_Combine_Networking/Networking/domain/place/entity/Place.java
@@ -0,0 +1,37 @@
+package COTATO_Combine_Networking.Networking.domain.place.entity;
+
+import COTATO_Combine_Networking.Networking.global.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Place extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String placeName;
+
+    private String addressName;
+
+    private String roadAddressName;
+
+    private String longitude; // x (경도)
+
+    private String latitude; // y (위도)
+
+    @Builder
+    public Place(String placeName, String addressName, String roadAddressName, String longitude, String latitude) {
+        this.placeName = placeName;
+        this.addressName = addressName;
+        this.roadAddressName = roadAddressName;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    } ;
+}
