97202a0cc7e3773da4cc8b22d2f2af9bc734abf6
marshmallowing <yuin1111801@naver.com>
2025-05-19 13:49:40 +0900

    fix: @OpenAPIDefinition 추가

--- src/main/java/COTATO_Combine_Networking/Networking/NetworkingApplication.java
+++ src/main/java/COTATO_Combine_Networking/Networking/NetworkingApplication.java
@@ -1,8 +1,15 @@
 package COTATO_Combine_Networking.Networking;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "http://3.133.128.168:8080", description = "Production Server"),
+		}
+)
 @SpringBootApplication
 public class NetworkingApplication {
 
